7804938a4094eced92256e13e04eaf42c101ab2e
HooraFarhangi99 <h_farhangi@comp.iust.ac.ir>
2020-07-08 17:18:18 +0430

    edit defs.h to prototype setpri function

--- defs.h
+++ defs.h
@@ -121,6 +121,7 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 int             waitx(int *,int *);
+int             setpri(int, int);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
